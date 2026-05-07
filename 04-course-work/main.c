72c53940149b160eab78f5e0467825f5f8c782ba
shafed <shaparenko.fedor@gmail.com>
2026-05-07 14:57:41 +0300

    add swapped

--- 04-course-work/main.c
+++ 04-course-work/main.c
@@ -36,13 +36,17 @@ void printArray(int arr[], int n) { // Функция для вывода мас
 
 void bubbleSort(int arr[], int n) { // Функция для сортировки
   for (int i = 0; i < n - 1; i++) {
+    int swapped = 0; // Флаг наличия обменов на текущем проходе
     for (int j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         int temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        swapped = 1;
       }
     }
+    if (!swapped)
+      break;
   }
 }
 
